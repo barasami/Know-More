f69b590f1a85a1ded4be83e7275a35f6d0a84d27
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-09 22:01:25 +0300

    updated  nav

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -10,15 +10,15 @@ function Navigation() {
     <div className='nav'>
         <div className='navigation'>
             <div className='links'>
-              <Link to='/' className='innerlinks'>daily word<div className='icon'>
-                <LanguageIcon/></div></Link>
+              <Link to='/' className='innerlinks'><div className='link'>daily word</div><div className='icon'>
+              <LanguageIcon/></div></Link>
             </div>
             <div className='links'>
-              <Link to='/Countryfact' className='innerlinks'>Country Facts<div className='icon'>
+              <Link to='/Countryfact' className='innerlinks'><div className='link'>Country Facts</div><div className='icon'>
                 <FactCheckIcon/></div></Link>
             </div>
             <div className='links'>
-              <Link to='/Quotes' className='innerlinks'>Daily Quote<div className='icon'>
+              <Link to='/Quotes' className='innerlinks'><div className='link'>Daily Quote</div><div className='icon'>
                 <FormatQuoteIcon /></div></Link>
             </div>
         </div>
