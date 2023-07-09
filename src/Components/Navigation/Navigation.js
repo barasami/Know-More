ce3e24841644ce36d34d6ddfd2f4858d13b23907
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-09 21:46:56 +0300

    fixed nav

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
-// import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
-// import FactCheckIcon from '@mui/icons-material/FactCheck';
-// import LanguageIcon from '@mui/icons-material/Language';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import FactCheckIcon from '@mui/icons-material/FactCheck';
+import LanguageIcon from '@mui/icons-material/Language';
 import { Link } from 'react-router-dom';
 import './Nav.css'
 
@@ -10,13 +10,16 @@ function Navigation() {
     <div className='nav'>
         <div className='navigation'>
             <div className='links'>
-              <Link to='/' className='innerlinks'>daily word</Link>
+              <Link to='/' className='innerlinks'>daily word<div className='icon'>
+                <LanguageIcon/></div></Link>
             </div>
             <div className='links'>
-              <Link to='/Countryfact' className='innerlinks'>Country Facts</Link>
+              <Link to='/Countryfact' className='innerlinks'>Country Facts<div className='icon'>
+                <FactCheckIcon/></div></Link>
             </div>
             <div className='links'>
-              <Link to='/Quotes' className='innerlinks'>Daily Quote</Link>
+              <Link to='/Quotes' className='innerlinks'>Daily Quote<div className='icon'>
+                <FormatQuoteIcon /></div></Link>
             </div>
         </div>
     </div>
